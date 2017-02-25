b0247e5c690d73095f7d6ec607f90340d95666ea
rwblickhan <rwblickhan@gmail.com>
2017-02-25 12:03:08 -0800

    Improve file server

--- index.js
+++ index.js
@@ -6,24 +6,32 @@ var fs = require('fs');
 http.createServer(listener).listen(8000);
 
 function listener(req, resp) {
+    //determine local path to requested resource
     var localFile = path.join(process.cwd(), url.parse(req.url).pathname);
-   
+    var contentType = path.extname(localFile).substr(1); //substr to remove extra '.' from extname
+    if (localFile === path.dirname(localFile)) {
+        //hacky way to redirect directory to {dir}/index.html
+        localFile += 'index.html';
+    } 
+
     fs.open(localFile, 'r', (err, fd) => {
         if (err) {
+            //file not found
             resp.writeHead(404, {'Content-Type': 'text/plain'});
             resp.write("404 Not Found\n");
             resp.end();
             return; 
         }
-        fs.readFile(fd, 'binary', (err, data) => {
+        fs.readFile(fd, 'utf8', (err, data) => {
             if (err) {
+                //incorrect file conversion
                 resp.writeHead(500, {'Content-Type': 'text/plain'});
                 resp.write(err + '\n');
                 resp.end();
                 return;    
             }
-            resp.writeHead(200, {'Content-Type': 'text/html'});
-            resp.write(data, 'binary');
+            resp.writeHead(200, {'Content-Type': 'text/' + contentType});
+            resp.write(data, 'utf8');
             resp.end();
         });
     });
