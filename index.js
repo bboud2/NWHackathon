5a4fa9f3b80344a92712ddc18cf215c595fc8d5b
rwblickhan <rwblickhan@gmail.com>
2017-02-25 10:59:46 -0800

    Basic html-serving functionality in server

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 var http = require('http');
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
 
-var server = http.createServer(listener);
-server.listen(8000);
+http.createServer(listener).listen(8000);
 
-function listener(req, rep)  {
-    console.log('Hi!');
+function listener(req, resp) {
+    var localFile = path.join(process.cwd(), url.parse(req.url).pathname);
+   
+    fs.open(localFile, 'r', (err, fd) => {
+        if (err) {
+            resp.writeHead(404, {'Content-Type': 'text/plain'});
+            resp.write("404 Not Found\n");
+            resp.end();
+            return; 
+        }
+        fs.readFile(fd, 'binary', (err, data) => {
+            if (err) {
+                resp.writeHead(500, {'Content-Type': 'text/plain'});
+                resp.write(err + '\n');
+                resp.end();
+                return;    
+            }
+            resp.writeHead(200, {'Content-Type': 'text/html'});
+            resp.write(data, 'binary');
+            resp.end();
+        });
+    });
 }
