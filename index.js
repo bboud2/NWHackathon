2a574c8a90c2a18572a92ba6b55bfea17424566e
Giulia <giuliamattia1@gmail.com>
2017-02-25 10:53:08 -0800

    added google maps stuff

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by giuliamattia on 2017-02-25.
+ */
+
+
+$(function () {
+
+    $("#submit").click(function (e) {
+        console.log('clicked submit');
+        // request
+    });
+
+
+
+});
