266dc42b2829cfbdfa547f2183026468a2e55bba
rwblickhan <rwblickhan@gmail.com>
2017-02-26 12:35:38 -0800

    Split converter off into its own script

--- converter.js
+++ converter.js
@@ -1,8 +1,32 @@
-var converter = require('xml2json');
 var path = require('path');
 var fs = require('fs');
-var filename = 'doc.kml';
-var inFile = path.join(process.cwd(), filename);
-var outFile = path.join(process.cwd(), 'doc.json');
 
-fs.writeFileSync(outFile, converter.toJson(fs.readFileSync(inFile)));
+function convert(filename) {
+   var inFilename = filename + '.csv';
+   var inFile = path.join(process.cwd(), '/mapdata', inFilename);
+   var inFileContents = fs.readFileSync(inFile, 'utf8');
+   var lines = inFileContents.split('\r\n');
+   var result = [];
+   var headers = lines[0].split(',');
+   for (var i = 1; i < lines.length; i++) {
+        var obj = {};
+        var currentLine = lines[i].split(',');
+        for (var j = 0; j < headers.length; j++) {
+            obj[headers[j]] = currentLine[j];
+        }
+        result.push(obj);
+   }
+
+   var outFilename = filename +'.json';
+   var outFile = path.join(process.cwd(), '/mapdata', outFilename);
+   fs.writeFileSync(outFile, JSON.stringify(result));
+   console.log('Done converting ' + filename);
+}
+
+convert('BUS_ROUTES');
+convert('BUS_STOPS');
+convert('PARKS');
+convert('SKYTRAIN_STATIONS_PTS');
+convert('TRAFFIC_VOLUMES');
+convert('TREES_EAST');
+convert('TREES_WEST');
--- index.js
+++ index.js
@@ -3,28 +3,6 @@ var url = require('url');
 var path = require('path');
 var fs = require('fs');
 
-function convert(filename) {
-   var inFilename = filename + '.csv';
-   var inFile = path.join(process.cwd(), '/mapdata', inFilename);
-   var inFileContents = fs.readFileSync(inFile, 'utf8');
-   var lines = inFileContents.split('\r\n');
-   var result = [];
-   var headers = lines[0].split(',');
-   for (var i = 1; i < lines.length; i++) {
-        var obj = {};
-        var currentLine = lines[i].split(',');
-        for (var j = 0; j < headers.length; j++) {
-            obj[headers[j]] = currentLine[j];
-        }
-        result.push(obj);
-   }
-
-   var outFilename = filename +'.json';
-   var outFile = path.join(process.cwd(), '/mapdata', outFilename);
-   fs.writeFileSync(outFile, JSON.stringify(result));
-   console.log('Done converting ' + filename);
-}
-
 function listener(req, resp) {
     //determine local path to requested resource
     var localFile = path.join(process.cwd(), url.parse(req.url).pathname);
@@ -57,12 +35,5 @@ function listener(req, resp) {
     });
 }
 
-convert('BUS_ROUTES');
-convert('BUS_STOPS');
-convert('PARKS');
-convert('SKYTRAIN_STATIONS_PTS');
-convert('TRAFFIC_VOLUMES');
-convert('TREES_EAST');
-convert('TREES_WEST');
 http.createServer(listener).listen(8000);
 
